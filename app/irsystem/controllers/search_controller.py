ed35af1151d4d19c92ede13addcb54b2fb17de36
Seraphina Lee <el542@cornell.edu>
2018-04-09 22:06:23 -0400

    cos_sim and test populating some data

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,23 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+import json
 
 project_name = "TasteTest"
 net_ids = "Robert Li: rl597, Seraphina Lee: el542, Frank Li: fl338, Stephen Ye: xy93"
+
+
+#######delete later, just populating test flavor values
+import re
+
+with open('aggreg.json') as json_data:
+	raw = json.load(json_data)
+flav_mat = np.zeros((len(raw), 5))
+flav_norms = np.zeros(len(raw))
+for i in range(len(raw)):
+	flav_mat[i] = [i%10]*5
+	flav_norms[i] = np.linalg.norm(flav_mat[i])
+#######
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -12,9 +26,17 @@ def search():
 		data = []
 		output_message = ''
 	else:
+		##query should be of form '2,5,7,2,9' (5 integers separated out by commas) for now
 		output_message = "Your search: " + query
-		data = range(5)
+		flav = re.findall('[0-9]+', query)
+		flavors = np.array([int(x) for x in flav])
+		data = [raw[i]['title'] for i in cos_sim_flavor(flavors)]
 	return render_template('search.html', name=project_name, netid=net_ids, output_message=output_message, data=data)
 
 
-
+#precompute flavor matrix (recipe x flavor) + docnorms (recipe x norm)
+#takes in np array of np array containing flavor profile, and returns a ranked list of indices
+def cos_sim_flavor(flavors):
+	lst = np.dot(flav_mat,flavors)
+	scores = np.divide(lst, flav_norms)
+	return np.ndarray.tolist(np.argsort(-scores))
