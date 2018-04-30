75a55f2a86899b6dd109930c868ad4fa7c1e9bdf
Robert Li <robertli@dhcp-rhodes-668.redrover.cornell.edu>
2018-04-30 17:54:51 -0400

    last one probably

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -24,6 +24,24 @@ net_ids = "Robert Li: rl597, Seraphina Lee: el542, Frank Li: fl338, Steven Ye: x
 #import units
 #import ml_model
 
+smallest = ["pinch", "pinches", "gram",  "mL", "grams", "g", "ml",  "gr", "gm", "dash", "Dash", "dashes",  "Gram"]
+spoon = ["teaspoons", "tablespoon", "teaspoon", "tablespoons", "tsp", "Tb", "tbsp", "Tablespoon",
+	"Teaspoon", "T", "t", "Tbsp", "Tbs", "Tablespoons", "tsps", "Tsp","Teaspoons", "tbsps", "TB", "Tbsps"]
+cup = ["cup", "cups", "c", "C", "Cup", "Cups"]
+pint = ["pint", "pints", "pt"]
+small = ["ounces", "oz", "ounce", "fl. oz.", "ozs", "fluid ounces", "Oz"]
+bigger = ["pounds", "lb", "pound", "lbs", "kg", "Pound", "Pounds", "quarts", "quart", "l", "liters", "L", "liter"]
+length = ["9-inch", "7-inch", "3-inch", "6-inch", "2-inch", "4-inch", "10-inch", "8 inch", "inches", "8-inch","inch"]
+biggest = ["gallon"]
+
+units_lst = [(smallest, 0.1), (spoon, 1), (cup, 1), (pint, 4), (small, 0.2), (bigger, 8), (length, 0.5), (biggest, 16)]
+
+def unit_weights(unit):
+	for (lst, weight) in units_lst:
+		if unit in lst:
+			return weight
+	return 1
+
 annotatedDict = {"button mushrooms" : { "umami" : 8, "bitter" : 2, "sour" : 1, "salty": 1, "sweet" : 1 },
 			 "poppy seeds" : { "umami" : 1, "bitter" : 2, "sour" : 1, "salty": 2, "sweet" : 3 },
 			 "low sodium chicken stock" : { "umami" : 6, "bitter" : 1, "sour" : 1, "salty": 4, "sweet" : 2 },
@@ -617,24 +635,8 @@ def search():
 		                                  restrictions=restrictions,
 		                                  output_message=output_message)
 
-smallest = ["pinch", "pinches", "gram",  "mL", "grams", "g", "ml",  "gr", "gm", "dash", "Dash", "dashes",  "Gram"]
-spoon = ["teaspoons", "tablespoon", "teaspoon", "tablespoons", "tsp", "Tb", "tbsp", "Tablespoon",
-	"Teaspoon", "T", "t", "Tbsp", "Tbs", "Tablespoons", "tsps", "Tsp","Teaspoons", "tbsps", "TB", "Tbsps"]
-cup = ["cup", "cups", "c", "C", "Cup", "Cups"]
-pint = ["pint", "pints", "pt"]
-small = ["ounces", "oz", "ounce", "fl. oz.", "ozs", "fluid ounces", "Oz"]
-bigger = ["pounds", "lb", "pound", "lbs", "kg", "Pound", "Pounds", "quarts", "quart", "l", "liters", "L", "liter"]
-length = ["9-inch", "7-inch", "3-inch", "6-inch", "2-inch", "4-inch", "10-inch", "8 inch", "inches", "8-inch","inch"]
-biggest = ["gallon"]
-
-units_lst = [(smallest, 0.1), (spoon, 1), (cup, 1), (pint, 4), (small, 0.2), (bigger, 8), (length, 0.5), (biggest, 16)]
 
 
-def unit_weights(unit):
-	for (lst, weight) in units_lst:
-		if unit in lst:
-			return weight
-	return 1
 #precompute flavor matrix (recipe x flavor) + docnorms (recipe x norm)
 #takes in np array of np array containing flavor profile, and returns a ranked list of indices
 def cos_sim_flavor(flavors, mat):
